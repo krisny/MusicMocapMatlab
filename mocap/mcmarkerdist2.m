19ca767bdc2e4596144a0c30fbe254752c24b548
krisny <krisny@ifi.uio.no>
2018-09-18 13:08:13 +0200

    minor update. remove some semicolons and obolete help text

--- mocap/mcmarkerdist2.m
+++ mocap/mcmarkerdist2.m
@@ -16,15 +16,15 @@ function dist = mcmarkerdist2(d, m1, m2)
 %
 % (Updates by Kristian Nymoen, University of Oslo. Work in progress:
 % - m2 may be an array of two markers, giving the distance between marker 
-%   m1 and a line spanned by two other markers (implemented)
+%   m1 and a line spanned by two other markers
 % - m2 may be an array of three markers, giving the distance between 
-%   marker m1 and a plane spanned by the three other markers (not implemented yet)
+%   marker m1 and a plane spanned by the three other markers
 %
 % ? Part of the Motion Capture Toolbox, Copyright ?2008, 
 % University of Jyvaskyla, Finland
 
 if isfield(d,'type') && strcmp(d.type, 'MoCap data')
-    if length(m2) == 1;
+    if length(m2) == 1
         %calc distance between two markers
         if m1>d.nMarkers || m2>d.nMarkers || m1<1 || m2<1 
             disp('Marker numbers out of range.'); 
@@ -32,7 +32,7 @@ if isfield(d,'type') && strcmp(d.type, 'MoCap data')
         end
         c1 = 3*m1+(-2:0); c2 = 3*m2+(-2:0); 
         dist = sqrt(sum((d.data(:,c1)-d.data(:,c2)).^2,2));
-    elseif length(m2) == 2;
+    elseif length(m2) == 2
         %calc distance between marker and line segment
         if m1>d.nMarkers || max(m2>d.nMarkers) || m1<1 || max(m2<1)
             disp('Marker numbers out of range.'); 
@@ -43,7 +43,7 @@ if isfield(d,'type') && strcmp(d.type, 'MoCap data')
         for i = 1:d.nFrames
             dist(i) = norm(cross([d.data(i,c2);d.data(i,c3)]',[d.data(i,c2);d.data(i,c1)]'))/norm([d.data(i,c2);d.data(i,c3)]');
         end
-    elseif length(m2) == 3;
+    elseif length(m2) == 3
         %calc distance between marker and plane
         if m1>d.nMarkers || max(m2>d.nMarkers) || m1<1 || max(m2<1)
             disp('Marker numbers out of range.'); 
