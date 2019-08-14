290413d73c0067afa8220997a1e6077a4b087257
krisny <krisny@ifi.uio.no>
2019-08-14 19:05:28 +0200

    allow mcarrayMean to handle norm structs

--- mocap/mcarrays/mcarrayMean.m
+++ mocap/mcarrays/mcarrayMean.m
@@ -4,20 +4,23 @@ function meanOut = mcarrayMean(d)
 % If the number of frames in the structs are different, the longer structs
 % will be cut.
 %
+% All entries must have the same number of markers
+%
 % By Kristian Nymoen, RITMO/University of Oslo, 2019
 %
 
     
     dframes = min([d.nFrames]);
     dmarkers = max([d.nMarkers]);
+    ddim = size(d(1).data,2);
 
     dl = length(d);
 
-    q = nan(dframes,dmarkers*3,dl);
+    q = nan(dframes,ddim,dl);
 
     for i = 1:dl
 
-        q(1:dframes,1:1:d(i).nMarkers*3,i) = d(i).data(1:dframes,:);
+        q(1:dframes,1:ddim,i) = d(i).data(1:dframes,:);
 
     end
 
